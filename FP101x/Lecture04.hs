26b72f0406e931576e13489e8a76d24c0428fee6
Dmitry Ratushnyy <d.ratushnyy@gmail.com>
2019-10-12 14:56:39 +0900

    Added samples from lecture 4

---
+++ FP101x/Lecture04.hs
@@ -0,0 +1,31 @@
+-- list comprehensions
+concat :: [[a]] -> [a]
+concat xss = [x | xs <- xss, x <- xs]
+
+firsts :: [(a, b)] -> [a]
+firsts ps = [x | (x, _) <- ps]
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+-
+-- naive prime
+prime :: Int -> Bool
+{-
+ note that "False" will be returned as soon as  any factor other than
+ 1 or n will be produced
+-}
+prime n = factors n == [1, n]
+
+find :: Eq a => a -> [(a, b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
+
+pairs :: [a] -> [(a, a)]
+pairs xs = zip xs (tail xs)
+
+sorted :: Ord a => [a] -> Bool
+sorted xs = and [x <= y | (x, y) <- pairs xs]
+
+positions :: Eq a => a -> [a] -> [Int]
+-- note that [1..] is generates infinite list
+-- but it will stop once "zip" exhausts "xs"
+positions x xs = [i | (x',i) <- zip xs [0..], x == x']
